c3770b13e89529bcebed3274f78899f1e35cfe94
roelvanegmond <36486184+roelvanegmond@users.noreply.github.com>
2024-04-02 14:16:04 +0200

    Adding debug

--- src/lib/SQLLiteDataGenerator.js
+++ src/lib/SQLLiteDataGenerator.js
@@ -5,9 +5,10 @@ class SQLLiteDataGenerator {
    * @param {string} database - The path to the SQLite database file.
    * @param {object} sqlite3 - The SQLite3 module.
    */
-  constructor(database, sqlite3 = require("sqlite3").verbose()) {
+  constructor(database, sqlite3 = require("sqlite3").verbose(), debug = require("debug")("sqllite-data-generator")) {
     this.sqlite3 = sqlite3;
     this.db = new sqlite3.Database(database);
+    this.debug = debug;
   }
 
   connect() {
@@ -35,6 +36,7 @@ class SQLLiteDataGenerator {
       const createTableQuery = `CREATE TABLE IF NOT EXISTS ${table.name} (${table.fields
         .map((field) => (field.name ? field.name + " " + field.type : field.type))
         .join(", ")})`;
+      this.debug(`Executing create query: ${createTableQuery}`);
       this.db.run(createTableQuery, (err) => {
         if (err) {
           reject(`Error creating table ${table.name}: ${err.message}`);
@@ -55,7 +57,7 @@ class SQLLiteDataGenerator {
             try {
               return `"${await field.generator()}"`;
             } catch (e) {
-              reject(`Error generating data for field ${field.name} in table ${table.name}: ${e.message}`);
+              reject(`Error generating data for field ${field.name} in table ${table.name}: ${e?.message || e}`);
             }
           } else {
             return "NULL";
@@ -65,6 +67,8 @@ class SQLLiteDataGenerator {
         insertQuery += `(${rowValues.join(", ")}), `;
       }
       insertQuery = insertQuery.slice(0, -2); // Remove the last comma and space
+
+      this.debug(`Executing insert query: ${insertQuery}`);
 
       this.db.run(insertQuery, (err) => {
         if (err) {
@@ -76,9 +80,10 @@ class SQLLiteDataGenerator {
     });
   }
 
-  getRandomIdFromTable(tableName) {
+  getRandomIdFromTable(tableName, filter = "") {
     return new Promise((resolve, reject) => {
-      const query = `SELECT id FROM ${tableName} ORDER BY RANDOM() LIMIT 1`;
+      const query = `SELECT id FROM ${tableName} ${filter ? "WHERE " + filter : ""} ORDER BY RANDOM() LIMIT 1`;
+      this.debug(`Executing select query: ${query}`);
       this.db.get(query, (err, row) => {
         if (err) {
           reject(`Error getting random ID from table ${tableName}: ${err.message}`);
